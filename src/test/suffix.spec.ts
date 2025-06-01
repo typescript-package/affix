03b5ffc35949818c26a1e092a6d4c66a09b3ebb4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-01 02:56:38 +0000

    test: update.

--- src/test/suffix.spec.ts
+++ src/test/suffix.spec.ts
@@ -3,5 +3,4 @@ import { Suffix } from "../lib";
 const suffix = new Suffix();
 
 console.log(suffix);
-console.log(`typeOf(), `, typeOf(suffix));
 console.log(`[object Suffix], `, Object.prototype.toString.call(suffix));
