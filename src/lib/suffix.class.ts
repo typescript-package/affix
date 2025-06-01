23baffb65fdca78344714a5e3343bf0bb83dbf68
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-01 02:42:22 +0000

    refactor(`Suffix`): `Value` defaults to an empty `string`, add `Pattern` generic type variable

--- src/lib/suffix.class.ts
+++ src/lib/suffix.class.ts
@@ -4,10 +4,38 @@ import { Affix } from "./affix.abstract";
  * @description A class to manage suffixes that can be applied to strings.
  * @export
  * @class Suffix
- * @template {string} [Value=string] The type of suffix constrained by the `string`.
- * @extends {Affix<Value>}
+ * @template {string} [Value=''] The type of suffix constrained by the `string`.
+ * @template {RegExp | string | undefined} [Pattern=RegExp | string | undefined] 
+ * @extends {Affix<Value, 'suffix', Pattern>}
  */
-export class Suffix<Value extends string = string> extends Affix<Value> {
+export class Suffix<
+  Value extends string = '',
+  Pattern extends RegExp | string | undefined = RegExp | string | undefined,
+> extends Affix<Value, 'suffix', Pattern> {
+  /**
+   * @inheritdoc
+   * @public
+   * @static
+   * @type {RegExp}
+   */
+  public static override pattern: RegExp | string = super.pattern;
+
+  /**
+   * @inheritdoc 
+   * @public
+   * @static
+   * @template {string} [Value=''] 
+   * @param {Value} value 
+   * @param {(RegExp | string)} [pattern=Suffix.pattern] 
+   * @returns {Value} 
+   */
+  public static override sanitize<Value extends string = ''>(
+    value: Value,
+    pattern: RegExp | string = Suffix.pattern
+  ): Value {
+    return value.replace(pattern, '') as Value;
+  }
+
   /**
    * @description Returns the `string` tag representation of the `Suffix` class when used in `Object.prototype.toString.call(instance)`.
    * @public
@@ -15,28 +43,19 @@ export class Suffix<Value extends string = string> extends Affix<Value> {
    * @type {string}
    */
   public override get [Symbol.toStringTag]() {
-    return 'Suffix';
+    return Suffix.name;
   }
 
   /**
-   * @description Sanitizes the suffix with a `pattern`.
-   * @public
-   * @param {string} value 
-   * @param {RegExp} [pattern=Suffix.pattern] 
-   * @returns {string} 
+   * Creates an instance of `Suffix`.
+   * @constructor
+   * @param {Value} [value='' as Value] The value of the suffix, constrained by the `string` type. Defaults to an empty string.
+   * @param {(Pattern)} [pattern=Suffix.pattern] The pattern to sanitize the suffix. Defaults to the static `Suffix.pattern`.
    */
-  public static override sanitize<Value extends string = string>(
-    value: Value,
-    pattern: RegExp = Suffix.pattern
-  ): Value {
-    return value.replace(pattern, '') as Value;
+  constructor(
+    value: Value = '' as Value,
+    pattern: Pattern = Suffix.pattern as Pattern
+  ) {
+    super(value, { kind: 'suffix', pattern });
   }
-
-  /**
-   * @inheritdoc
-   * @public
-   * @static
-   * @type {RegExp}
-   */
-  public static override pattern: RegExp = super.pattern;
 }
