ff6af3a0deafa5d4abb551fd2d5bac1e8d7406e9
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-01 02:42:51 +0000

    test(`Affix`): update.

--- src/test/affix.spec.ts
+++ src/test/affix.spec.ts
@@ -1,10 +1,19 @@
-import { typeOf } from "@typescript-package/core";
+import { BasicAffixKind } from "@typedly/affix";
 import { Affix } from "../lib";
 
-export class TestAffix extends Affix {}
+export class TestAffix<
+  Value extends string = '',
+  KindValue extends BasicAffixKind | undefined = BasicAffixKind,
+  PatternValue extends RegExp | string | undefined = RegExp | string | undefined,
+> extends Affix<Value, KindValue, PatternValue> {}
 
-const testAffix = new TestAffix();
+const testAffix = new TestAffix("testAffixValue", 
+  {
+    kind: 'prefix',
+    pattern: /[^a-zA-Z0-9$_]/g,
+  }
+  // {'min': 3, 'max': 10}
+);
 
 console.log(testAffix);
-console.log(`typeOf(), `, typeOf(testAffix));
 console.log(`[object Affix], `, Object.prototype.toString.call(testAffix).match(/\[object (\w+)]/)?.[1]);
