e88b1a927e4cfc9730ea3f1e22c3ff7bfe044634
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-09 19:03:51 +0000

    refactor(`Suffix`): add `append()` static method, and `appendTo()` `set()` methods, getter `suffix()`

--- src/lib/suffix.class.ts
+++ src/lib/suffix.class.ts
@@ -1,5 +1,7 @@
 // Class.
 import { Affix } from "./affix.class";
+// Type.
+import { SuffixTemplate } from "@typedly/affix";
 /**
  * @description A class to manage suffixes that can be applied to strings.
  * @export
@@ -13,28 +15,45 @@ export class Suffix<
   Pattern extends RegExp | string | undefined = RegExp | string | undefined,
 > extends Affix<Value, 'suffix', Pattern> {
   /**
-   * @inheritdoc
+   * @description Appends the suffix to the stem string.
    * @public
    * @static
-   * @type {RegExp}
+   * @template {string} [Stem=string] The type of stem string constrained by the `string`.
+   * @template {string} [Suffix=string] The type of suffix constrained by the `string`.
+   * @param {Stem} stem The stem string to append the suffix.
+   * @param {Suffix} [suffix=Suffix.default as Suffix] Suffix to append to stem.
+   * @param {Delimiter} [delimiter='' as Delimiter] The delimiter to use between the stem and the suffix.
+   * @param {boolean} [sanitize=true] Whether to sanitize the suffix using the static `Suffix.pattern`.
+   * @returns {SuffixTemplate<Stem, Suffix>} 
    */
-  public static override pattern: RegExp | string = super.pattern;
+  public static append<
+    Stem extends string = string,
+    Suffix extends string = string,
+    Delimiter extends string = ''
+  >(
+    stem: Stem,
+    suffix: Suffix = Suffix.default as Suffix,
+    delimiter: Delimiter = '' as Delimiter,
+    sanitize: boolean = true
+  ): SuffixTemplate<Stem, Suffix, Delimiter> {
+    return `${stem}${delimiter}${sanitize ? super.sanitize(suffix, this.pattern) : suffix}`;
+  }
 
   /**
-   * @inheritdoc 
+   * @description Tag name for the `toStringTag`.
    * @public
    * @static
-   * @template {string} [Value=''] 
-   * @param {Value} value 
-   * @param {(RegExp | string)} [pattern=Suffix.pattern] 
-   * @returns {Value} 
+   * @type {string}
    */
-  public static override sanitize<Value extends string = ''>(
-    value: Value,
-    pattern: RegExp | string = Suffix.pattern
-  ): Value {
-    return value.replace(pattern, '') as Value;
-  }
+  public static override tagName: string = 'Suffix';
+
+  /**
+   * @description The default suffix to use.
+   * @public
+   * @static
+   * @type {string}
+   */
+  public static default: string = '';
 
   /**
    * @description Returns the `string` tag representation of the `Suffix` class when used in `Object.prototype.toString.call(instance)`.
@@ -43,7 +62,17 @@ export class Suffix<
    * @type {string}
    */
   public override get [Symbol.toStringTag]() {
-    return Suffix.name;
+    return Suffix.tagName;
+  }
+
+  /**
+   * @description
+   * @public
+   * @readonly
+   * @type {Value}
+   */
+  public get suffix(): Value {
+    return this.value as Value;
   }
 
   /**
@@ -57,5 +86,42 @@ export class Suffix<
     pattern: Pattern = Suffix.pattern as Pattern
   ) {
     super(value, { kind: 'suffix', pattern });
+  }
+
+  /**
+   * @description Appends the internal suffix to the stem string.
+   * @public
+   * @template {string} Stem The type of stem string.
+   * @param {Stem} stem The stem string to append the suffix.
+   * @param {Delimiter} delimiter The delimiter between stem and suffix.
+   * @returns {SuffixTemplate<Stem, Value, Delimiter>} The returned value is a template of stem and suffix.
+   */
+  public appendTo<
+    Stem extends string,
+    Delimiter extends string = ''
+  >(
+    stem: Stem,
+    delimiter: Delimiter = '' as Delimiter
+  ): SuffixTemplate<Stem, Value, Delimiter> {
+    return Suffix.append(
+      stem,
+      Suffix.sanitize(this.suffix, this.pattern),
+      delimiter,
+      false
+    );
+  }
+  
+  /**
+   * Sets the pattern and value for the suffix affix.
+   * @inheritdoc
+   * @public
+   * @param {{ pattern?: Pattern, value?: Value }} [param0={}] 
+   * @param {Pattern} param0.pattern The pattern to sanitize the suffix.
+   * @param {Value} param0.value The value of the suffix, constrained by the `string` type.
+   * @returns {this} The current instance of `Suffix`.
+   */
+  public override set({ pattern, value }: { pattern?: Pattern, value?: Value } = {}): this {
+    super.set({ pattern, value });
+    return this;
   }
 }
