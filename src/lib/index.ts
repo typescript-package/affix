d7ff0bdbb0257d6388ec2d50f49f8c254ae4b5a4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-09 19:04:11 +0000

    chore(index): add `Circumfix` and `Infix`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,7 @@
 export { AffixCore } from './affix-core.abstract';
 // Class.
 export { Affix } from './affix.class';
+export { Circumfix } from './circumfix.class';
+export { Infix } from './infix.class';
 export { Prefix } from './prefix.class';
 export { Suffix } from './suffix.class';
